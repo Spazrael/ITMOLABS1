5989f5659933f48809a94bffb4a74b4140e9dadb
Spazrael <daniil127@mail.ru>
2022-12-26 18:48:46 +0300

    SomeFixes

--- Lab4/Main.cpp
+++ Lab4/Main.cpp
@@ -1,56 +1,57 @@
 #include <iostream>
 #include <fstream>
-#include <stdarg.h>
-#include <random>
 #include <string>
-#include <U:\VSPROJECTS\Lab4\Dice.h>
+#include <random>
+#include <stdarg.h>
 
-int GetDice(std::string dice)
+using namespace std;
+
+int dGet(string dice)
 {
 	int result = 0;
 	int Throws = 0;
 	int dType = 0;
 	int dMod = 0;
-	bool isPlus = false;
-	bool isMinus = false;
+	bool isPl = false;
+	bool isM = false;
 
-	std::string str = "";
+	string str = "";
 	for (auto ch : dice)
 	{
 		str += ch;
 
 		if (ch == 'd')
 		{
-			Throws = std::stoi(str);
+			Throws = stoi(str);
 			str = "";
 		}
 		else if (ch == '+')
 		{
-			isPlus = true;
-			dType = std::stoi(str);
+			isPl = true;
+			dType = stoi(str);
 			str = "";
 		}
 		else if (ch == '-')
 		{
-			isMinus = true;
-			dType = std::stoi(str);
+			isM = true;
+			dType = stoi(str);
 			str = "";
 		}
 	}
 
-	if (isPlus) dMod += std::stoi(str);
-	else if (isMinus) dMod -= std::stoi(str);
-	else dType = std::stoi(str);
+	if (isPl) dMod += stoi(str);
+	else if (isM) dMod -= stoi(str);
+	else dType = stoi(str);
 
-	for (int i = 0; i < Throws; i++) result += std::rand() % dType + 1;
+	for (int i = 0; i < Throws; i++) result += rand() % dType + 1;
 	result += dMod;
 
 	return result;
 }
 
-std::vector<int> ThrowDice(int dAmount...)
+vector<int> dThrow(int dAmount...)
 {
-	std::vector<int> results;
+	vector<int> results;
 
 	va_list dices;
 	char* dice;
@@ -59,7 +60,7 @@ std::vector<int> ThrowDice(int dAmount...)
 	for (int i = 0; i < dAmount; i++)
 	{
 		dice = va_arg(dices, char*);
-		results.push_back(GetDice(dice));
+		results.push_back(dGet(dice));
 	}
 	va_end(dices);
 
@@ -68,37 +69,37 @@ std::vector<int> ThrowDice(int dAmount...)
 int main()
 {
 
-    std::srand(time(NULL));
+	srand(time(NULL));
 
-    int D1d6[6] = {};
-    int D2d6[12] = {};
-    int D3d6[18] = {};
-    int D1d10[10] = {};
-    int D2d10[20] = {};
-    int D3d10[30] = {};
+	int D1d6[6] = {};
+	int D2d6[12] = {};
+	int D3d6[18] = {};
+	int D1d10[10] = {};
+	int D2d10[20] = {};
+	int D3d10[30] = {};
 
-    for (int i = 0; i < 100000; i++)
-    {
-        std::cout << i + 1 << " throw\n";
-        D1d6[ThrowDice(1, "1d6").at(0) - 1]++;
-        D2d6[ThrowDice(1, "2d6").at(0) - 1]++;
-        D3d6[ThrowDice(1, "3d6").at(0) - 1]++;
-        D1d10[ThrowDice(1, "1d10").at(0) - 1]++;
-        D2d10[ThrowDice(1, "2d10").at(0) - 1]++;
-        D3d10[ThrowDice(1, "3d10").at(0) - 1]++;
-    }
-    std::ofstream os("stats.txt");
-    for (int i = 0; i < 6; i++) os << D1d6[i] << "\t";
-    os << "\n";
-    for (int i = 0; i < 12; i++) os << D2d6[i] << "\t";
-    os << "\n";
-    for (int i = 0; i < 18; i++) os << D3d6[i] << "\t";
-    os << "\n";
-    for (int i = 0; i < 10; i++) os << D1d10[i] << "\t";
-    os << "\n";
-    for (int i = 0; i < 20; i++) os << D2d10[i] << "\t";
-    os << "\n";
-    for (int i = 0; i < 30; i++) os << D3d10[i] << "\t";
-    os << "\n"; 
-    os.close();
+	for (int i = 0; i < 100000; i++)
+	{
+		cout << i + 1 << " throw\n";
+		D1d6[dThrow(1, "1d6").at(0) - 1]++;
+		D2d6[dThrow(1, "2d6").at(0) - 1]++;
+		D3d6[dThrow(1, "3d6").at(0) - 1]++;
+		D1d10[dThrow(1, "1d10").at(0) - 1]++;
+		D2d10[dThrow(1, "2d10").at(0) - 1]++;
+		D3d10[dThrow(1, "3d10").at(0) - 1]++;
+	}
+	ofstream os("stats.txt");
+	for (int i = 0; i < 6; i++) os << D1d6[i] << "\t";
+	os << "\n";
+	for (int i = 0; i < 12; i++) os << D2d6[i] << "\t";
+	os << "\n";
+	for (int i = 0; i < 18; i++) os << D3d6[i] << "\t";
+	os << "\n";
+	for (int i = 0; i < 10; i++) os << D1d10[i] << "\t";
+	os << "\n";
+	for (int i = 0; i < 20; i++) os << D2d10[i] << "\t";
+	os << "\n";
+	for (int i = 0; i < 30; i++) os << D3d10[i] << "\t";
+	os << "\n";
+	os.close();
 }
